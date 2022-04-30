ae18cb234f0862e4fd7219214ebcd8e83a27d1e8
islahatun <islahatunnufusi98@gmail.com>
2022-05-01 02:30:16 +0700

    first Commit

---
+++ layouts/default.vue
@@ -0,0 +1,94 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" fixed app>
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in sideMenu"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar fixed app>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-bottom-navigation horizontal height="10vh" fixed color="primary" app>
+      <v-app-bar-nav-icon
+        @click.stop="drawer = !drawer"
+        v-ripple="false"
+        plain
+      />
+      <v-btn
+        v-for="(item, i) in bottomMenu"
+        :key="i"
+        :to="item.to"
+        v-ripple="false"
+        plain
+      >
+        <span>{{ item.title }}</span>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+      drawer: false,
+      sideMenu: [
+        {
+          icon: 'mdi-application',
+          title: 'Apps',
+          to: '/',
+        },
+        {
+          icon: 'mdi-account',
+          title: 'Account',
+          to: '/account',
+        },
+        {
+          icon: 'mdi-bell',
+          title: 'Notification',
+          to: '/notification',
+        },
+      ],
+      bottomMenu: [
+        {
+          icon: 'mdi-application',
+          title: 'Apps',
+          to: '/',
+        },
+        {
+          icon: 'mdi-account',
+          title: 'Account',
+          to: '/account',
+        },
+        {
+          icon: 'mdi-bell',
+          title: 'Notification',
+          to: '/notification',
+        },
+      ],
+      title: 'Toko Online',
+    }
+  },
+}
+</script>
